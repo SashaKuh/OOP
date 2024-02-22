55be02c91aef27478fecd469fa40ab29e46d6fb1
tye1o <135231311+tye1o@users.noreply.github.com>
2024-02-22 08:46:29 +0200

    Add files via upload

---
+++ denys_khaliuk/lab3.py
@@ -0,0 +1,34 @@
+# використання умовних операцій
+
+a = 1
+if a > 0:
+    print("a>0")
+elif a < 0:
+    print("a<0")
+else:
+    print("0")
+
+# оператори булевої логіки
+
+y = 6
+if y > 5 and y < 7:
+    print("y(5:7) ")
+
+# складні умови (проектація)
+
+age = 27
+if age >= 27 and age <= 65:
+    print("fine!)")
+
+# Використання циклу for 
+
+for i in range(5):
+    print("iteration_numb", i)
+
+
+# Використання циклу while 
+
+numb = 1
+while numb <= 5:
+    print(numb)
+    numb += 1
