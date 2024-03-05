73d6a67ba5f17490c0b13df357e306f424142525
RoZ1n7 <vasachepenak@gmail.com>
2024-03-05 18:45:45 +0200

    lab2

--- Vasil_Chepeniak/lab-2/script.py
+++ Vasil_Chepeniak/lab-2/script.py
@@ -1,2 +1,43 @@
-a = 10
-print(a)
+#приклади того як задаюься змінні
+zminna  = 10
+zminna_str = '5'
+a, b, c = 1, 2, 3
+
+
+#приклад зміни типу змінної
+zminna_str = int(zminna_str)
+
+
+#приклад роботи з списками
+
+my_list = [1, 2, 3]
+my_list.append(4)
+my_list.pop(0)
+print(my_list[0])  # Вивід: 2
+
+#базові опратори з числовими змінними
+
+a = 5
+b = 2
+result1 = a + b 
+result2 = a - b 
+result3 = a * b  
+result4 = a / b 
+result5 = a // b
+result6 = a % b  
+result6 = a ** b  
+
+#робота над стрічками
+
+name = "John"
+name = name + ' !'
+name = name.lower()
+name = name.upper()
+name = name.split()
+greeting = f"Hello, {name}!"
+
+#словники
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+value = my_dict['key1']
+
