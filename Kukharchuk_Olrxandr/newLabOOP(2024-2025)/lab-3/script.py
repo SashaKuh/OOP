81c14c16fed3b2f50cfcb4cd5d401af7658f1e39
Oleksandr Kukharchuk <71312391+SashaKuh@users.noreply.github.com>
2024-10-15 09:35:28 +0300

    Update script.py

--- Kukharchuk_Olrxandr/newLabOOP(2024-2025)/lab-3/script.py
+++ Kukharchuk_Olrxandr/newLabOOP(2024-2025)/lab-3/script.py
@@ -1,48 +1,21 @@
-class Publisher:
-    def __init__(self):
-        self._subscribers = [] 
+class MyIterator:
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
 
-    def subscribe(self, subscriber):
-        self._subscribers.append(subscriber)
+    def __iter__(self):
+        return self
 
-    def unsubscribe(self, subscriber):
-        self._subscribers.remove(subscriber)
+    def __next__(self):
+        if self.index < len(self.data):
+            result = self.data[self.index]
+            self.index += 1
+            return result
+        else:
+            raise StopIteration
 
-    def notify(self, message):
-        for subscriber in self._subscribers:
-            subscriber.update(message)
+my_list = [1, 2, 3, 4, 5]
+iterator = MyIterator(my_list)
 
-
-class Subscriber:
-    def update(self, message):
-        raise NotImplementedError("Subclasses must implement update method")
-
-
-class EmailSubscriber(Subscriber):
-    def __init__(self, name):
-        self.name = name
-
-    def update(self, message):
-        print(f"Email to {self.name}: {message}")
-
-
-class SMSSubscriber(Subscriber):
-    def __init__(self, phone_number):
-        self.phone_number = phone_number
-
-    def update(self, message):
-        print(f"SMS to {self.phone_number}: {message}")
-
-store = Publisher()
-
-email_subscriber = EmailSubscriber("sasha@gmail.com")
-sms_subscriber = SMSSubscriber("+1234567890")
-
-store.subscribe(email_subscriber)
-store.subscribe(sms_subscriber)
-
-store.notify("Новий товар доступний в магазині!")
-
-store.unsubscribe(sms_subscriber)
-
-store.notify("Інше повідомлення для залишених підписників.")
+for item in iterator:
+    print(item)
