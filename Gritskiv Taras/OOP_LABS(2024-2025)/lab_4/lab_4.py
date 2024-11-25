3946c0d7509064d6e69acc693ec714d3bfb3f47d
Tarasgr7 <114420340+Tarasgr7@users.noreply.github.com>
2024-11-25 08:23:51 +0200

    Update lab_4.py

--- Gritskiv Taras/OOP_LABS(2024-2025)/lab_4/lab_4.py
+++ Gritskiv Taras/OOP_LABS(2024-2025)/lab_4/lab_4.py
@@ -12,14 +12,14 @@ class VideoDevice(ABC):
 
 class Radio(AudioDevice):
     def play_audio(self):
-        print("Playing audio on the radio Запустити аудіо на радіо")
+        print(" Запустити аудіо на радіо")
 
 class Television(AudioDevice, VideoDevice):
     def play_audio(self):
-        print("Playing audio on the television Запустити аудіо на телебаченні")
+        print(" Запустити аудіо на телебаченні")
 
     def play_video(self):
-        print("Playing video on the television Запустити відео на телебаченні")
+        print(" Запустити відео на телебаченні")
 
 # Використання
 def start_audio(device: AudioDevice):
