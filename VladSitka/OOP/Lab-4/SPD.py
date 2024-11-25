3514474581e28a23d71207e586a1942fdc51f574
VladSitka <157682250+VladSitka@users.noreply.github.com>
2024-11-25 09:41:39 +0200

    Update SPD.py

--- VladSitka/OOP/Lab-4/SPD.py
+++ VladSitka/OOP/Lab-4/SPD.py
@@ -7,7 +7,11 @@ class Order:
         return sum([item['price'] * self.quantity[i] for i, item in enumerate(self.items)])
 
 class OrderPersistence:
-    
-    def save_to_file(order, filename):
+    def __init__(self,order):
+        self.order = order
+
+    def save_to_file(self, filename):
         with open(filename, 'w') as file:
             file.write(f"Items: {order.items}\nQuantity: {order.quantity}\nTotal: {order.calculate_total()}")
+
+order = Order (items=[{'name':'Яблуко', 'price': 1.2}, {'name':'banana','price':0.8}], quantity=[3,5])
